08858b68c0ecab03ae4033821deab71d298ecee8
bruno pini <78656526+brunopini@users.noreply.github.com>
2024-04-14 19:50:47 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function storeValues() {
+    const name = document.getElementById('name').value;
+    const advertiserId = document.getElementById('advertiser_id').value;
+
+    console.log("Stored values:", name, advertiserId);
+    // Here, we're just logging the values to the console.
+    // You can later modify this to do something else with the values.
+}
